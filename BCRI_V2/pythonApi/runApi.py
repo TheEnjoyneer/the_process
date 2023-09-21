8c4d9fc22c2e8a17131bc4d9a203ae19cb7f01ed
Christopher Brant <cbrant@enjoyneering.org>
2023-09-21 16:57:42 -0400

    updated python api with gameCards and numGames

--- BCRI_V2/pythonApi/runApi.py
+++ BCRI_V2/pythonApi/runApi.py
@@ -8,22 +8,63 @@ import getopt
 import argparse
 from flask import Flask, jsonify 
 from flask_cors import CORS, cross_origin
-
-
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/topGame")
+# Return total number of games for this week
+@app.route("/numGames")
 @cross_origin(supports_credentials=True)
-def getTopGame():
-	matchupStr = weekGamesOrdered[0].awayTeamRank + " at " + weekGamesOrdered[0].homeTeamRank
-	confStr = weekGamesOrdered[0].awayCon + " vs " + weekGamesOrdered[0].homeCon
+def getNumGames():
 	data = {
-		'matchup': matchupStr,
-		'conferences': confStr,
-		'venue': weekGamesOrdered[0].venue
+		'numGames': len(weekGamesOrdered)
 	}
 	return jsonify(data)
+
+@app.route("/gameCards")
+@cross_origin(supports_credentials=True)
+def getGameCards():
+	gameCards = []
+	for game in weekGamesOrdered:
+		matchupStr = game.awayTeamRank + " at " + game.homeTeamRank
+		confStr = game.awayCon + " vs " + game.homeCon
+		if len(game.lines) != 0:
+			provider = game.lines[0].provider
+			moneyline = game.lines[0].home_moneyline
+			currSpread = game.lines[0].formatted_spread
+			if game.lines[0].spread_open > 0:
+				openSpread = game.awayTeam + " -" + str(game.lines[0].spread_open)
+			elif game.lines[0].spread_open < 0: 
+				openSpread = game.homeTeam + " " + str(game.lines[0].spread_open)
+			else:
+				openSpread = "Pick 'em"
+			if (game.lines[0].over_under_open is not None) and (game.lines[0].over_under is not None):
+				currTotal = game.lines[0].over_under
+				openTotal = game.lines[0].over_under_open
+			else:
+				currTotal = None
+				openTotal = None
+		else:
+			provider = "N/A"
+			openSpread = "N/A"
+			currSpread = "N/A"
+			currTotal = "N/A"
+			openTotal = "N/A"
+			moneyline = "N/A"	
+		data = {
+			'gameID': game.gameID,
+			'matchup': matchupStr,
+			'conferences': confStr,
+			'venue': game.venue,
+			'provider' : provider,
+			'openSpread': openSpread,
+			'currSpread': currSpread,
+			'currTotal': currTotal,
+			'openTotal': openTotal,
+			'moneyline': moneyline 
+		}
+		gameCards.append(data)
+	return jsonify(gameCards)
 
 if __name__ == '__main__':
 	# Create ordered games data on server side
