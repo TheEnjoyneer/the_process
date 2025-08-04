d104560bf3c3636fb11c6d75787fcb7b87ff623c
Christopher Brant <cdbrant@enjoyneering.org>
2025-08-04 15:24:50 -0400

    test update

--- cfbStats/cfbStatsTest.py
+++ cfbStats/cfbStatsTest.py
@@ -4,6 +4,15 @@ import cfbStatsLib as api
 
 testWeek = api.getCalendarWeek(2024, 1, "regular")
 
-for weekList in testWeek:
-	print(weekList)
-
+print("------------- Week Nights -------------")
+print(testWeek[0])
+print("------------- Saturday Early -------------")
+print(testWeek[1])
+print("------------- Saturday Afternoon -------------")
+print(testWeek[2])
+print("------------- Saturday Evening -------------")
+print(testWeek[3])
+print("------------- Saturday Late -------------")
+print(testWeek[4])
+print("------------- Extra Nights -------------")
+print(testWeek[5])
