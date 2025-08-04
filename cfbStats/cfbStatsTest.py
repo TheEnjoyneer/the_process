4ec6fe8fc522a8e313e0456bde5066b86270cc8f
Christopher Brant <cdbrant@enjoyneering.org>
2025-08-04 15:21:57 -0400

    test update

--- cfbStats/cfbStatsTest.py
+++ cfbStats/cfbStatsTest.py
@@ -4,7 +4,6 @@ import cfbStatsLib as api
 
 testWeek = api.getCalendarWeek(2024, 1, "regular")
 
-testGame = testWeek['game'][-1]
-
-testGameDate = testGame['startDate']
+for weekList in testWeek:
+	print(weekList)
 
